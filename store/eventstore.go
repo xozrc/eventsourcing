1917f52d4a28a9e654e1a77036e4a696be2578a9
U-WAYNE\wayne <WAYNE+wayne@wayne.netherfire.com>
2016-06-12 10:11:49 +0800

    initial commit

--- store/eventstore.go
+++ store/eventstore.go
@@ -2,13 +2,15 @@ package store
 
 //event data
 type EventData struct {
-	Id            int64 `gorm:"primary_key"`
-	PartitionKey  string
+	Id int64 `gorm:"primary_key"`
+
 	SourceId      string
 	Version       int64
-	SourceType    string
 	Payload       string
 	CorrelationId string
+	EventType     string
+	PartitionKey  string
+	SourceType    string
 }
 
 func (ed *EventData) TableName() string {
